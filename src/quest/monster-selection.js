30ec9038cbaf945f67dcbc8c37cac7b1f7d30d59
Rodrigo Santos da Silva <rodrigo.saint01@live.com>
2018-01-09 18:34:31 -0200

    Filtering mobs on mob selection.

--- src/quest/monster-selection.js
+++ src/quest/monster-selection.js
@@ -9,26 +9,40 @@ const MonsterSelectionDisplay = props =>
     </div>)
 }
 
+const mobCollection = [{"name": "Mob 1", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
+{"name": "Mob 2", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
+{"name": "Mob 3", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
+{"name": "Mob 4", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
+{"name": "Mob 5", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
+{"name": "Mob 6", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
+{"name": "Mob 7", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"}]
+
 class MonsterSelection extends React.Component
 {
     constructor(props)
     {
         super(props);
         this.state = {
-            mobCollection: [{"name": "Mob 1", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
-            {"name": "Mob 2", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
-            {"name": "Mob 3", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
-            {"name": "Mob 4", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
-            {"name": "Mob 5", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
-            {"name": "Mob 6", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"},
-            {"name": "Mob 7", "description": "It is a dragon that breaths fire and has sharp claws. It is a difficult task only selected for the best heroes.", "image": "http://cyanyurikago.web.fc2.com/images/cuelebre.png?16970772"}]        
+            filter: "",
+            mobCollection: mobCollection
         }
+    }
+
+    get mobCollectionFiltered()
+    {
+        if(!this.state.filter) return this.state.mobCollection;
+        return this.state.mobCollection.filter(mob => mob.name.toLowerCase().indexOf(this.state.filter) != -1)
+    }
+
+    updateFilterText(event)
+    {
+        this.setState({filter: event.target.value});
     }
 
     render()
     {
-        const mobCollection = this.state
-            .mobCollection.map(mob => <MonsterSelectionDisplay {...mob} />);
+        const mobSelectorComponentCollection = this.mobCollectionFiltered
+            .map(mob => <MonsterSelectionDisplay key={mob.name} {...mob} />);
         return (<div>
             <label htmlFor="modal_1" className="button">Select mob</label>
             <div className="modal">
@@ -41,8 +55,9 @@ class MonsterSelection extends React.Component
                     </header>
                     <div style={{padding: "1em"}}>
                         <div className="flex one">
-                            <input v-model="filter" type="text" placeholder="Filter"/>
-                            {mobCollection}
+                            <input value={this.state.filter} onInput={event => this.updateFilterText(event)} 
+                                type="text" placeholder="Filter"/>
+                            {mobSelectorComponentCollection}
                         </div>
                     </div>
                 </div>
